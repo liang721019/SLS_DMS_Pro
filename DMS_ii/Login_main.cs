bd3e8f3c25ca632550ac531534cc2df9f7f490e8
liang721019 <eric721019@gmail.com>
2017-01-26 13:58:34 +0800

    20170126_1358

--- DMS_ii/Login_main.cs
+++ DMS_ii/Login_main.cs
@@ -50,25 +50,35 @@ namespace DMS_ii
         {
             if (DMS_Login_ServerCB.Text != "")
             {
-                App_LoginPW = fun.desEncrypt_A(DMS_Login_PWD_tb.Text, "naturalbiokeyLogin");
-                fun.Query_DB = @"exec [TEST_SLSYHI].[dbo].[SLS_DMS_Login] '" +
-                                    DMS_Login_ID_tb.Text +
-                                    @"','" + App_LoginPW + "'";
-                fun.ProductDB_ds(fun.Query_DB);
-                if (fun.ds_index.Tables[0].Rows.Count != 0)
+                if (DMS_Login_ServerCB.Text == "PRD")
                 {
-                    //MessageBox.Show("登入成功!!");
-                    FileManager FM = new FileManager();
-                    FM.DMS_Service_ENV = DMS_Login_ServerCB.Text;       //server
-                    FM.DMS_UID = DMS_Login_ID_tb.Text;          //使用者ID
-                    this.Hide();
-                    FM.ShowDialog(this);
-                    this.Close();
+                    #region 內容
+                    App_LoginPW = fun.desEncrypt_A(DMS_Login_PWD_tb.Text, "naturalbiokeyLogin");
+                    fun.Query_DB = @"exec [TEST_SLSYHI].[dbo].[SLS_DMS_Login] '" +
+                                        DMS_Login_ID_tb.Text +
+                                        @"','" + App_LoginPW + "'";
+                    fun.ProductDB_ds(fun.Query_DB);
+                    if (fun.ds_index.Tables[0].Rows.Count != 0)
+                    {
+                        //MessageBox.Show("登入成功!!");
+                        FileManager FM = new FileManager();
+                        FM.DMS_Service_ENV = DMS_Login_ServerCB.Text;       //server
+                        FM.DMS_UID = DMS_Login_ID_tb.Text;          //使用者ID
+                        this.Hide();
+                        FM.ShowDialog(this);
+                        this.Close();
+                    }
+                    else
+                    {
+                        MessageBox.Show("帳密不正確!!", this.Text);
+                    }
+                    #endregion
                 }
                 else
                 {
-                    MessageBox.Show("帳密不正確!!", this.Text);
+                    MessageBox.Show("伺服器目前沒開放!!\n請選擇其他伺服器", this.Text);
                 }
+                
             }
             else
             {
